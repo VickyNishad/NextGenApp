ffa0bbc77e2898d4d2f1dcbd64efa361ea378358
NextGen2024 <76159587+VickyNishad@users.noreply.github.com>
2024-09-13 01:28:04 +0530

    change package name
    change package name

--- ms-email-mobile-sms-service/src/test/java/com/nextgen/sms/MsEmailMobileSmsServiceApplicationTests.java
+++ ms-email-mobile-sms-service/src/test/java/com/nextgen/sms/MsEmailMobileSmsServiceApplicationTests.java
@@ -1,6 +1,7 @@
-package com.ngr.server;
+package com.nextgen.sms;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
